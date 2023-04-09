6fe60a6715ac5c636ba7536d4404a4bb0deacd71
hamsa <hamsaalsaadie@gmail.com>
2023-04-09 20:28:50 +0300

    create Clothes Suggester repository

---
+++ app/src/main/java/com/example/clothes_suggester/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clothes_suggester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
