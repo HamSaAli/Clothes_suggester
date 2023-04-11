8786af51a68927e2f25745ab9d418bece7be2b59
hamsa <hamsaalsaadie@gmail.com>
2023-04-11 13:41:37 +0300

    add function to select random image

--- app/src/main/java/com/example/clothes_suggester/MainActivity.kt
+++ app/src/main/java/com/example/clothes_suggester/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.clothes_suggester
 
+import android.content.Context
+import android.content.SharedPreferences
 import okhttp3.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,58 +9,86 @@ import com.example.clothes_suggester.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val client = OkHttpClient()
     private val converter = WeatherConverter()
     private val apiManager = ApiManager(client, converter)
+    private lateinit var sharedPreferences: SharedPreferences
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         apiManager.getWeather(10.33f, 12.44f, ::onWeatherResponse, ::onError)
     }
 
-    private fun onWeatherResponse(temperature : Int){
+    private fun onWeatherResponse(temperature: Int) {
         runOnUiThread {
-            binding.tempText.text = "$temperature C"
+            binding.textTempature.text = "$temperature °C"
+
             setClothingImage(temperature)
         }
     }
 
-    private fun onError(){
+    private fun onError() {
 
     }
 
     private fun setClothingImage(temperature: Int) {
         val clothingList = when {
             temperature <= 0 -> listOf(
-                Clothing(R.drawable.hoodies),
-                Clothing(R.drawable.bag),
-                Clothing(R.drawable.hot1),
-                Clothing(R.drawable.hoodi)
+                Clothing(R.drawable.hoodie_white),
+                Clothing(R.drawable.jacket_brown),
+                Clothing(R.drawable.sweater_skyblue),
+                Clothing(R.drawable.sweater_green),
+                Clothing(R.drawable.jeans_brown),
+                Clothing(R.drawable.jeans_black),
             )
             temperature in 1..19 -> listOf(
-                Clothing(R.drawable.cold),
-                Clothing(R.drawable.jeans),
-                Clothing(R.drawable.white),
-                Clothing(R.drawable.bag)
+                Clothing(R.drawable.hoodie_white),
+                Clothing(R.drawable.jacket_brown),
+                Clothing(R.drawable.sweater_skyblue),
+                Clothing(R.drawable.sweater_green),
+                Clothing(R.drawable.jeans_brown),
+                Clothing(R.drawable.jeans_black),
             )
             temperature in 20..29 -> listOf(
-                Clothing(R.drawable.bag),
-                Clothing(R.drawable.hoodi),
-                Clothing(R.drawable.jeans),
-                Clothing(R.drawable.cold)
+                Clothing(R.drawable.dress_black),
+                Clothing(R.drawable.skirt_blue_white_jacket),
+                Clothing(R.drawable.skirt_black),
+                Clothing(R.drawable.skirt_blue),
+                Clothing(R.drawable.jeans_brown),
+                Clothing(R.drawable.jeans_black),
             )
             else -> listOf(
-                Clothing(R.drawable.hoodies),
-                Clothing(R.drawable.hot1),
-                Clothing(R.drawable.white),
-                Clothing(R.drawable.jeans)
-            )
+                Clothing(R.drawable.tshirt_white),
+                Clothing(R.drawable.tshirt_green),
+                Clothing(R.drawable.tshirt_pink),
+                Clothing(R.drawable.tshirt_brown),
+                Clothing(R.drawable.tshirt_striped_red),
+                Clothing(R.drawable.jeans_brown),
+                Clothing(R.drawable.jeans_black),
+                Clothing(R.drawable.shirt),
+                Clothing(R.drawable.jeans_black),
+                Clothing(R.drawable.dress_black),
+                Clothing(R.drawable.dress_orange),
+                Clothing(R.drawable.skirt_blue),
+                Clothing(R.drawable.skirt_black),
+                )
         }
 
         val randomClothing = clothingList.random()
+        val imageResourceId = randomClothing.imageResourceId
+
+
+        if (sharedPreferences.contains("lastImageResourceId") && sharedPreferences.getInt("lastImageResourceId", 0) == imageResourceId) {
+            setClothingImage(temperature)
+            return
+        }
         binding.clothesImage.setImageResource(randomClothing.imageResourceId)
+        sharedPreferences.edit().putInt("lastImageResourceId", imageResourceId).apply()
     }
 }
