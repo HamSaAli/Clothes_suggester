703b15bccd59579db38a3c7b206ceb26a8e5db42
hamsa <hamsaalsaadie@gmail.com>
2023-04-11 13:46:23 +0300

    remove unnecessary models

--- app/src/main/java/com/example/clothes_suggester/ApiManager.kt
+++ app/src/main/java/com/example/clothes_suggester/ApiManager.kt
@@ -6,7 +6,12 @@ import java.io.IOException
 import kotlin.reflect.KFunction1
 
 class ApiManager(private val client: OkHttpClient, private val converter: WeatherConverter) {
-    fun getWeather(latitude: Float, longitude: Float, onResult: KFunction1<Int, Unit>, onFailure: ()-> Unit) {
+    fun getWeather(
+        latitude: Float,
+        longitude: Float,
+        onResult: KFunction1<Int, Unit>,
+        onFailure: () -> Unit
+    ) {
         val request = Request.Builder()
             .url("$BASE_URL/weather?lat=$latitude&lon=$longitude&appid=$API_KEY")
             .build()
@@ -19,13 +24,16 @@ class ApiManager(private val client: OkHttpClient, private val converter: Weathe
                 response.body?.string()?.let { jsonString ->
                     val result = Gson().fromJson(jsonString, WeatherResponse::class.java)
                     val temperature =
-                        converter.convertFahrenheitToCelsius(result.main.temperature.toFloatOrNull() ?: 0f)
+                        converter.convertFahrenheitToCelsius(
+                            result.main.temperature.toFloatOrNull() ?: 0f
+                        )
                     onResult(temperature.toInt())
                 }
             }
         })
     }
-    companion object{
+
+    companion object {
         private const val BASE_URL = "https://api.openweathermap.org/data/2.5"
         private const val API_KEY = "3108fbcf03776def8491be32233fa864"
     }
--- app/src/main/java/com/example/clothes_suggester/WeatherResponse.kt
+++ app/src/main/java/com/example/clothes_suggester/WeatherResponse.kt
@@ -2,5 +2,6 @@ package com.example.clothes_suggester
 
 data class WeatherResponse(
     val name:String,
+    val main:Main,
     val wind:WindInfo
 )
