270be9232a1c8d8736fc182f5794ab24f16c4978
hamsa <hamsaalsaadie@gmail.com>
2023-04-13 08:11:52 +0300

    add location constant

--- app/src/main/java/com/example/clothes_suggester/utils/Constant.kt
+++ app/src/main/java/com/example/clothes_suggester/utils/Constant.kt
@@ -3,4 +3,7 @@ package com.example.clothes_suggester.utils
 object Constant {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5"
     const val API_KEY = "3108fbcf03776def8491be32233fa864"
+    const val MIN_TIME_BW_UPDATES: Long = 1000
+    const val REQUEST_LOCATION_PERMISSION = 1
+    const val MIN_DISTANCE_CHANGE_FOR_UPDATES: Float = 10f
 }
