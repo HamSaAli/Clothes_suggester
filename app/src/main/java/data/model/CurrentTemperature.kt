4cf32524981c2a9c341a7a92d7bb5d039a555a30
hamsa <hamsaalsaadie@gmail.com>
2023-04-19 00:54:19 +0300

    remove unnecessary permission and constant

--- app/src/main/java/data/model/CurrentTemperature.kt
+++
@@ -1,6 +0,0 @@
-package data.model
-
-data class CurrentTemperature(
-    val temperature :String ,
-    val weather_descriptions:List<String>,
-)
--- app/src/main/java/utils/Constant.kt
+++ app/src/main/java/utils/Constant.kt
@@ -3,6 +3,4 @@ package utils
 object Constant {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5"
     const val PERMISSION_PEQEST_ACCESS_LOCATION = 100
-    const val ICON_BASE_URL = "https://openweathermap.org/img/wn/"
-
 }
